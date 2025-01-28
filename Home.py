bf3dcacab37689f72515567689c6f6f8cc253d2c
“Volmir” <moderoso@gmail.com>
2025-01-27 22:02:44 -0300

    Menu Home

--- Home.py
+++ Home.py
@@ -16,6 +16,10 @@ st.title('ONG - Passos Mágicos :woman-woman-girl-boy:')
 
 
 with st.sidebar:
-    selected = option_menu("Main Menu", ["Home", 'Sobre'], 
-        icons=['house', 'gear'], menu_icon="cast", default_index=1)
-    selected
+    selected = option_menu("Menu", ["Home", 'Settings'], 
+        icons=['house', 'gear'], default_index=0)
+
+if selected == "Home":
+    st.write("home is where the heart is")
+else:
+    st.write("settings is my bettings")
