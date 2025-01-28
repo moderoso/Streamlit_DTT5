6517c9f89ba52a7a1e37ee86ca5db35518c0a6e7
“Volmir” <moderoso@gmail.com>
2025-01-27 22:00:58 -0300

    req

--- Home.py
+++ Home.py
@@ -6,6 +6,7 @@ import altair as alt
 from plotly.colors import n_colors
 import plotly.express as px
 import plotly.graph_objects as go
+from streamlit_option_menu import option_menu
 
 # Configuração da página
 st.set_page_config(page_title= 'ONG - Passos Mágicos', layout='wide', page_icon='📊')
@@ -13,10 +14,8 @@ st.set_page_config(page_title= 'ONG - Passos Mágicos', layout='wide', page_icon
 # Título da página
 st.title('ONG - Passos Mágicos :woman-woman-girl-boy:')
 
-import streamlit as st
-from streamlit_option_menu import option_menu
 
 with st.sidebar:
-    selected = option_menu("Main Menu", ["Home", 'Settings'], 
+    selected = option_menu("Main Menu", ["Home", 'Sobre'], 
         icons=['house', 'gear'], menu_icon="cast", default_index=1)
     selected
