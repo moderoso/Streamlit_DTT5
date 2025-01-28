4885ccda1a2008824d32a913d103777aaf396bb3
“Volmir” <moderoso@gmail.com>
2025-01-27 22:11:43 -0300

    Menu Home

--- Home.py
+++ Home.py
@@ -15,14 +15,14 @@ st.set_page_config(page_title= 'ONG - Passos Mágicos', layout='wide', page_icon
 st.title('ONG - Passos Mágicos :woman-woman-girl-boy:')
 
 
-with st.sidebar:
-    selected = option_menu("Menu", ["Home", 'Settings'], 
-        icons=['house', 'gear'], default_index=0)
+#with st.sidebar:
+ #   selected = option_menu("Menu", ["Home", 'Settings'], 
+ #       icons=['house', 'gear'], default_index=0)
 
-if selected == "Home":
-    st.write("home is where the heart is")
-else:
-    st.write("settings is my bettings")
+#if selected == "Home":
+#    st.write("home is where the heart is")
+#else:
+#    st.write("settings is my bettings")
 	
 	
 from streamlit_navigation_bar import st_navbar
