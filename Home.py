b02a782e1516045a025457d97300f19078797e37
“Volmir” <moderoso@gmail.com>
2025-01-27 22:41:40 -0300

    Menu Home

--- Home.py
+++ Home.py
@@ -16,36 +16,52 @@ st.set_page_config(page_title= 'ONG - Passos Mágicos', layout='wide', page_icon
 st.title('ONG - Passos Mágicos :woman-woman-girl-boy:')
 
 
-import streamlit as st
-from streamlit_navigation_bar import st_navbar
+from util.functions.path import get_file_path, get_dir_name, util_str, data_str
+
+from util.pages.home_page import home_page
+from util.pages.overview_page import overview_page
+from util.pages.pdb_page import pdb_page
+from util.pages.conformation_page import conformation_page
+from util.pages.mutation_page import mutation_page
+from util.pages.inhibitor_page import inhibitor_page
+from util.pages.query_page import query_page
+from util.pages.classify_page import classify_page
 
 
-st.set_page_config(initial_sidebar_state="collapsed")
+class MultiApp:
+    def __init__(self):
+        self.apps = []
 
-pages = ["Home", "Library", "Tutorials", "Development", "Download"]
-styles = {
-    "nav": {
-        "background-color": "rgb(123, 209, 146)",
-    },
-    "div": {
-        "max-width": "32rem",
-    },
-    "span": {
-        "border-radius": "0.5rem",
-        "color": "rgb(49, 51, 63)",
-        "margin": "0 0.125rem",
-        "padding": "0.4375rem 0.625rem",
-    },
-    "active": {
-        "background-color": "rgba(255, 255, 255, 0.25)",
-    },
-    "hover": {
-        "background-color": "rgba(255, 255, 255, 0.35)",
-    },
-}
+    def add_app(self, title, func):
+        self.apps.append({"title": title, "function": func})
 
-page = st_navbar(pages, styles=styles)
-st.write(page)
+    def run(self):
+        img = Image.open(
+            get_file_path(
+                "rascore_logo.png",
+                dir_path=f"{get_dir_name(__file__)}/{util_str}/{data_str}",
+            ),
+        )
 
-with st.sidebar:
-    st.write("Sidebar")
+        st.set_page_config(page_title="rascore", page_icon=img, layout="wide")
+
+        st.sidebar.markdown("## Main Menu")
+        app = st.sidebar.selectbox(
+            "Select Page", self.apps, format_func=lambda app: app["title"]
+        )
+        st.sidebar.markdown("---")
+        app["function"]()
+
+
+app = MultiApp()
+
+app.add_app("Home Page", home_page)
+app.add_app("Database Overview", overview_page)
+app.add_app("Search PDB", pdb_page)
+app.add_app("Explore Conformations", conformation_page)
+app.add_app("Analyze Mutations", mutation_page)
+app.add_app("Compare Inhibitors", inhibitor_page)
+app.add_app("Query Database", query_page)
+app.add_app("Classify Structures", classify_page)
+
+app.run()
