74b81d0a02881cc7698aa73a15439bee918cf44e
“Volmir” <moderoso@gmail.com>
2025-01-29 00:40:06 -0300

    top

--- Home.py
+++ Home.py
@@ -3,6 +3,10 @@ import streamlit as st
 from PIL import Image
 from auxiliar import apply_custom_style
 
+
+if __name__ == '__main__':
+        apply_custom_style()
+
 # Configuração da página
 st.set_page_config(page_title= 'ONG - Passos Mágicos', layout='wide', page_icon=':gem:')
 
