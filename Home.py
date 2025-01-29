668f024abc26ab40b64302fb03cbdb2fca059046
“Volmir” <moderoso@gmail.com>
2025-01-29 01:18:44 -0300

    top

--- Home.py
+++ Home.py
@@ -41,8 +41,8 @@ st.header(":footprints: ONG Passos Mágicos e seus principais indicadores")
 st.markdown("#")
 
 ### Tabs da página inicial
-tabs_titles_2= ["Como funciona a Passos","Alunos impactados pela Passos","Indicadores"]
-tabs_2 = st.tabs(tabs_titles_2)
+#tabs_titles_2= ["Como funciona a Passos","Alunos impactados pela Passos","Indicadores"]
+#tabs_2 = st.tabs(tabs_titles_2)
 
 # Tab Como funciona a Passos
 with tabs_2[0]: 
