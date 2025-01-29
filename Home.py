0f0c4d0179cb1418190b59120aed3f2befdb9964
“Volmir” <moderoso@gmail.com>
2025-01-29 01:02:05 -0300

    top

--- Home.py
+++ Home.py
@@ -10,6 +10,29 @@ if __name__ == '__main__':
 # Configuração da página
 #st.set_page_config(page_title= 'ONG - Passos Mágicos', layout='wide', page_icon=':gem:')
 
+
+###### Páginal Inicial do Streamlit ######
+st.set_page_config(layout= 'wide')
+
+#### Páginas
+cols = st.columns(6, gap="large")
+with cols[0]:
+    st.image("images/pm.png")
+with cols[1]:
+    if st.button("Home"):
+        st.switch_page("Home.py")
+with cols[2]:
+    if st.button("PSE (2020)"):
+        st.switch_page("pages/PSE (2020).py")
+with cols[3]:
+    if st.button("Sobre"):
+        st.switch_page("pages/MVP (sobre).py")
+with cols[4]:
+    if st.button("Referências"):
+        st.switch_page("pages/Sobre.py")
+
+st.header("", divider="gray")
+
 # Carregando top
 top = Image.open('images/top.png')
 st.image(top, width=200)
@@ -21,6 +44,8 @@ st.image(image, width=200)
 # Carregando top
 top2 = Image.open('images/top.png')
 st.image(top2, width=200)
+
+
 
 
 # Título da página
