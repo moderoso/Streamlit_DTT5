da1575afeeb99e6befae6980cb5c8d837d3f0e12
“Volmir” <moderoso@gmail.com>
2025-01-29 00:23:33 -0300

    aux

---
+++ auxiliar.py
@@ -0,0 +1,90 @@
+import streamlit as st
+import pandas as pd
+
+PRIMARY_COLOR = "#41c4f0"
+SECOND_COLOR = "#41aaf0"
+
+def apply_custom_style():
+    st_custom_style = """
+                <style>
+                summary {
+                    display: none;
+                }
+                h2 {
+                    color: {prim-color};
+                }
+                header[data-testid="stHeader"] {
+                    background-image: linear-gradient({prim-color}, {prim-color}, {prim-color});
+                }
+                div[data-testid="stStatusWidget"] div div div svg{
+                    color: white;
+                }
+                #relat-rio-de-exporta-o-de-vinhos, #tabela-resumida-com-informa-es-de-exporta-es-nos-ltimos-anos{
+                    text-align:center;
+                }
+                div[data-testid="stStatusWidget"] img{
+                    opacity: 100%;
+                }
+                div[data-testid="stStatusWidget"] label{
+                    color: white;
+                }
+                div[data-testid="stStatusWidget"] button{
+                    color: white;
+                    background: #9e829b;
+                    border-radius: 20px;
+                }
+                #MainMenu{
+                    color: white;
+                    visibility: hidden;
+                }
+                * {
+                  -webkit-user-drag: none;
+                  -khtml-user-drag: none;
+                  -moz-user-drag: none;
+                  -o-user-drag: none;
+                  user-drag: none;
+                }
+                a[href="#hide"] {
+                    visibility: hidden;
+                }
+                div[data-testid="stDecoration"] {
+                    background-image: none;
+                    background-color: black;
+                }
+                section>div.block-container {
+                    padding-top: 60px;
+                }
+                thead tr th:first-child {
+                    display:none
+                }
+                tbody th {
+                    display:none
+                }
+                .stAlert a {
+                    display: none;
+                }
+                button[role="tab"][aria-selected="true"] {
+                    background: #9e829b;
+                    padding: 4px;
+                    border-top-left-radius: 10px;
+                    border-top-right-radius: 10px;
+                    color: white;
+                }
+                div[data-testid="collapsedControl"] {
+                    color: white;
+                }
+                [tabindex="0"] > * {
+                
+                    max-width: 86rem !important;
+                }
+                #grupo {
+                    background: #9e829b;
+                    padding: 15px;
+                    border-radius: 10px;
+                }
+                </style>
+                """.replace('{prim-color}', PRIMARY_COLOR)
+    st_custom_style.replace('{second-color}', SECOND_COLOR)
+	
+
+    st.markdown(st_custom_style, unsafe_allow_html=True)
