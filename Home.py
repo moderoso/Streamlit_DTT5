0e28a70fed0327b4f20847743faf3ce1d151f81f
“Volmir” <moderoso@gmail.com>
2025-01-28 23:36:39 -0300

    back

--- Home.py
+++ Home.py
@@ -9,6 +9,6 @@ st.set_page_config(page_title= 'ONG - Passos Mágicos', layout='wide', page_icon
 #st.title('ONG - Passos Mágicos :woman-woman-girl-boy:✨')
 
 from PIL import Image
-image = Image.open('images/pm.png', width=1000, height=1500)
-st.image(image)
+image = Image.open('images/pm.png')
+st.image(image, width=1000, height=1500)
 
