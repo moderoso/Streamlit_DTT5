51ee5e4232c5df06c1e27130b436c9cda2b84048
“Volmir” <moderoso@gmail.com>
2025-01-28 23:32:03 -0300

    back

--- Home.py
+++ Home.py
@@ -8,3 +8,7 @@ st.set_page_config(page_title= 'ONG - Passos Mágicos', layout='wide', page_icon
 # Título da página
 st.title('ONG - Passos Mágicos :woman-woman-girl-boy:✨')
 
+from PIL import Image
+image = Image.open('pm.png')
+st.image(image)
+
