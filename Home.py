4ab0cdca179054077f63f4a49f06dae1a1383c6d
“Volmir” <moderoso@gmail.com>
2025-01-28 23:49:03 -0300

    back

--- Home.py
+++ Home.py
@@ -5,32 +5,10 @@ from PIL import Image
 # Configuração da página
 st.set_page_config(page_title= 'ONG - Passos Mágicos', layout='wide', page_icon=':gem:')
 
-# Título da página
-#st.title('ONG - Passos Mágicos :woman-woman-girl-boy:✨')
-
 # Carregando image
 image = Image.open('images/pm.png')
 st.image(image, width=200)
 
 # Título da página
 st.title('ONG - Passos Mágicos :woman-woman-girl-boy:✨')
-
-
-side_bg = 'images/pm.png'
-sidebar_bg(side_bg)
-
-def sidebar_bg(side_bg):
-
-   side_bg_ext = 'png'
-
-   st.markdown(
-      f"""
-      <style>
-      [data-testid="stSidebar"] > div:first-child {{
-          background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()});
-      }}
-      </style>
-      """,
-      unsafe_allow_html=True,
-      )
 
