bf57f3757a2c444a2d4f76b375c74c0ea7100ab4
“Volmir” <moderoso@gmail.com>
2025-01-29 00:18:34 -0300

    back

--- Home.py
+++ Home.py
@@ -1,6 +1,7 @@
 # Importação das bibliotecas
 import streamlit as st
 from PIL import Image
+from auxiliar import apply_custom_style
 
 # Configuração da página
 st.set_page_config(page_title= 'ONG - Passos Mágicos', layout='wide', page_icon=':gem:')
@@ -11,4 +12,28 @@ st.image(image, width=200)
 
 # Título da página
 st.title('ONG - Passos Mágicos :woman-woman-girl-boy:✨')
+
+if __name__ == '__main__':
+    st.set_page_config(page_title='Passos Magicos - Dashboard',
+                       layout='wide')
+    
+    st.image('./assets/Passos-magicos-icon-cor.png')
+    apply_custom_style()
+    
+    st.markdown("""
+        <style>
+            .section {
+                font-family: 'Sans-serif';
+                font-size: 20px;
+                font-weight: bold;
+                margin-bottom: 10px;
+            }
+            
+            .indicator {
+                margin-left: 20px;
+                font-size: 16px;
+                font-weight: normal;
+            }
+        </style>
+    """, unsafe_allow_html=True)
 
