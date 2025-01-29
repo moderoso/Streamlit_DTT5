16cb79aecd041c61c3eef4d6f7df689d78849f41
“Volmir” <moderoso@gmail.com>
2025-01-28 23:33:27 -0300

    back

--- Home.py
+++ Home.py
@@ -6,9 +6,9 @@ import streamlit as st
 st.set_page_config(page_title= 'ONG - Passos Mágicos', layout='wide', page_icon=':gem:')
 
 # Título da página
-st.title('ONG - Passos Mágicos :woman-woman-girl-boy:✨')
+#st.title('ONG - Passos Mágicos :woman-woman-girl-boy:✨')
 
 from PIL import Image
-image = Image.open('pm.png')
+image = Image.open('images/pm.png')
 st.image(image)
 
