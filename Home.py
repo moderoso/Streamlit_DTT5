931a6bb5a700efccb034a514af31911350d21234
“Volmir” <moderoso@gmail.com>
2025-01-28 23:03:11 -0300

    back

--- Home.py
+++ Home.py
@@ -30,4 +30,4 @@ def set_png_as_page_bg(png_file):
     st.markdown(page_bg_img, unsafe_allow_html=True)
     return
 
-set_png_as_page_bg('/mount/src/streamlit_dtt5/images/pm.png')
+set_png_as_page_bg('pm.png')
