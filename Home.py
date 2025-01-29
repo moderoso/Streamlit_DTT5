69dad80bf0ded50b3b98fa18cf773b62df207f9e
“Volmir” <moderoso@gmail.com>
2025-01-28 22:45:11 -0300

    back

--- Home.py
+++ Home.py
@@ -12,41 +12,16 @@ from streamlit_navigation_bar import st_navbar
 st.set_page_config(page_title= 'ONG - Passos Mágicos', layout='wide', page_icon=':gem:📊')
 
 # Título da página
-st.title('ONG - Passos Mágicos :woman-woman-girl-boy:')
+st.title('ONG - Passos Mágicos :woman-woman-girl-boy:✨')
 
-# ---- Background ----
-original_title = '<h1 style="font-family: serif; color:white; font-size: 20px;">Streamlit CSS Styling✨ </h1>'
-st.markdown(original_title, unsafe_allow_html=True)
-
-
-# Set the background image
-background_image = """
+page_bg_img = """
 <style>
-[data-testid="stAppViewContainer"] > .main {
-    background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
-    background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
-    background-position: center;  
-    background-repeat: no-repeat;
-}
+body {{
+background-image: url("images/pm.png");
+background-size: cover;
+}}
 </style>
 """
 
-st.markdown(background_image, unsafe_allow_html=True)
-
-st.text_input("", placeholder="Streamlit CSS ")
-
-input_style = """
-<style>
-input[type="text"] {
-    background-color: transparent;
-    color: #a19eae;  // This changes the text color inside the input box
-}
-div[data-baseweb="base-input"] {
-    background-color: transparent !important;
-}
-[data-testid="stAppViewContainer"] {
-    background-color: transparent !important;
-}
-</style>
-"""
-st.markdown(input_style, unsafe_allow_html=True)
+# Display the background image
+st.markdown(page_bg_img, unsafe_allow_html=True)
