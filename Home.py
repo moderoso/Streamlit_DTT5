105be1d301384dc43161887169f92c080c30be4e
“Volmir” <moderoso@gmail.com>
2025-01-28 23:20:36 -0300

    back

--- Home.py
+++ Home.py
@@ -14,16 +14,17 @@ st.set_page_config(page_title= 'ONG - Passos Mágicos', layout='wide', page_icon
 # Título da página
 st.title('ONG - Passos Mágicos :woman-woman-girl-boy:✨')
 
-side_bg_ext = 'pm.png'
-sidebar_bg(side_bg)
 
-st.markdown(
+side_bg_ext = 'pm.png'
+sidebar_bg(side_bg_ext)
+
+   st.markdown(
       f"""
       <style>
       [data-testid="stSidebar"] > div:first-child {{
-          background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()});
+          background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg_ext, "rb").read()).decode()});
       }}
       </style>
       """,
       unsafe_allow_html=True,
-)
+      )
