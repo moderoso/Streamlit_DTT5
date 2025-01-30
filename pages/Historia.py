9141ce5a57338ae9582aeaaed5034c311d7bb197
“Volmir” <moderoso@gmail.com>
2025-01-29 23:47:20 -0300

    historia

--- pages/Historia.py
+++ pages/Historia.py
@@ -27,3 +27,7 @@ with cols[4]:
     if st.button("Sobre"):
         st.switch_page("pages/Sobre.py")
 st.header("", divider="gray")
+
+# Descrição Passos Magicos
+st.markdown('### Quem Somos')
+st.markdown('<p style="text-align: justify;">A ONG Passos Mágicos é uma organização dedicada a transformar vidas por meio da educação e do acolhimento de crianças e jovens em situação de vulnerabilidade. Com um olhar atento às necessidades de cada estudante, a ONG oferece mais do que apenas ensino – ela proporciona um espaço seguro onde cada criança pode se sentir valorizada, respeitada e motivada a alcançar seu potencial.<r />A missão da Passos Mágicos é movida pela crença de que, para que um aluno realmente prospere, é necessário atender não só às suas necessidades acadêmicas, mas também ao seu bem-estar emocional, social e psicológico. A ONG se dedica a ser uma rede de apoio, não só no processo de aprendizagem, mas em todo o desenvolvimento do jovem, ajudando-o a superar desafios diários, sejam eles de ordem pessoal, familiar ou comunitária.<r />Em tempos de tantas desigualdades, a Passos Mágicos se destaca, oferecendo oportunidades para que cada criança e jovem possa dar os passos necessários para um futuro mais digno e promissor.Por isso, a Passos Mágicos busca entender e adaptar suas ações para apoiar de forma personalizada, promovendo um ambiente acolhedor onde a confiança e a autoestima possam florescer.</p>', unsafe_allow_html = True)
