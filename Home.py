4242c2302b3dbecf707b89d62bdf82af72ffd9fd
“Volmir” <moderoso@gmail.com>
2025-02-05 00:28:51 -0300

    modelo

--- Home.py
+++ Home.py
@@ -130,9 +130,20 @@ with tabs_2[3]:
     colunas_3 = st.columns(2, gap="large")
     st.markdown('<ul class="font-text-destaques"><p style="text-align: justify;"><li><span style="font-weight: bold">Random Forest: </span>Algoritmo de aprendizado de máquina baseado em conjuntos de árvores de decisão. Ele funciona criando várias árvores de decisão durante o treinamento e, em seguida, combinando suas previsões para melhorar a precisão do modelo e reduzir o risco de overfitting (ajuste excessivo aos dados).</li></p></ul>', unsafe_allow_html = True)
     st.markdown('<ul class="font-text-destaques"><p style="text-align: justify;"><li><span style="font-weight: bold">XGBoost (Extreme Gradient Boosting): </span>É um algoritmo de aprendizado de máquina altamente eficiente e poderoso, utilizado principalmente para classificação e regressão. Ele é uma implementação otimizada do método de Gradient Boosting, que combina múltiplos modelos fracos (tipicamente árvores de decisão) para formar um modelo forte.</li></p></ul>', unsafe_allow_html = True)
+    st.markdown( '#### Termos')
+    st.markdown('<p style="text-align: justify;">Durante a análise dos modelos, iremos utilizar alguns termos e para facilitar o entendimento do que estamos descrevendo e abaixo há uma melhor explicação de cada termo:</p>', unsafe_allow_html = True)
+    st.markdown('<ul class="font-text-destaques"><p style="text-align: justify;"><li><span style="font-weight: bold">Precision: </span>A taxa de acerto entre as previsões positivas feitas pelo modelo e as reais positivas.</li></p></ul>', unsafe_allow_html = True)
+    st.markdown('<ul class="font-text-destaques"><p style="text-align: justify;"><li><span style="font-weight: bold">Recall: </span>A taxa de acerto entre as reais positivas e as positivas corretamente identificadas pelo modelo.</li></p></ul>', unsafe_allow_html = True)	
+    st.markdown('<ul class="font-text-destaques"><p style="text-align: justify;"><li><span style="font-weight: bold">F1-Score: </span>A média harmônica entre precisão e recall, usada para balancear esses dois aspectos.</li></p></ul>', unsafe_allow_html = True)
+    st.markdown('<ul class="font-text-destaques"><p style="text-align: justify;"><li><span style="font-weight: bold">Support: </span> Quantidade de exemplos reais de cada classe no conjunto de dados de teste.</li></p></ul>', unsafe_allow_html = True)
+    st.markdown('<ul class="font-text-destaques"><p style="text-align: justify;"><li><span style="font-weight: bold">Accuracy: </span> A proporção de acertos no total de previsões feitas.</li></p></ul>', unsafe_allow_html = True)
+    st.markdown('<ul class="font-text-destaques"><p style="text-align: justify;"><li><span style="font-weight: bold">Macro Avg: </span>A média macro calcula a média simples de uma métrica (como precisão, recall ou F1-score) para todas as classes, sem levar em consideração o suporte de cada classe.</li></p></ul>', unsafe_allow_html = True)
+    st.markdown('<ul class="font-text-destaques"><p style="text-align: justify;"><li><span style="font-weight: bold">Weighted Avg: </span>A média ponderada calcula a média das métricas (como precisão, recall ou F1-score), pesando cada classe de acordo com o número de exemplos (suporte) da classe no conjunto de teste.</li></p></ul>', unsafe_allow_html = True)	
 	
-	
-	
+    st.markdown( '#### Comparação dos modelos')
+    st.markdown('<p style="text-align: justify;">Comparando o treinamento dos dois modelos, é possível obter as seguintes informações:</p>', unsafe_allow_html = True)	
+    st.image("images/mod_random.png",caption="Modelo Random Forest", width=600)
+    st.image("images/mod_XGBoost.png",caption="Modelo XGBoost", width=600)	
 	
 	
 ## Rodapé
