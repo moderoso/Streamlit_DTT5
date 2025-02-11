fe6e72ac11b43eb44e0928133adbd031a404ed3a
Jhonny Mineu <104777793+JhonnyMineu@users.noreply.github.com>
2025-02-11 18:50:12 -0300

    Update 04 Dashboard.py

--- pages/04 Dashboard.py
+++ pages/04 Dashboard.py
@@ -39,6 +39,6 @@ st.header("", divider="gray")
 # Título da página
 st.title('Dashboard e Storytelling')
 
-powerbi_url = 'https://app.powerbi.com/view?r=eyJrIjoiMmZjOGI5MjgtMDE0NS00ZDgyLWE1MDAtZjExMjhkNThjOTRjIiwidCI6IjExZGJiZmUyLTg5YjgtNDU0OS1iZTEwLWNlYzM2NGU1OTU1MSIsImMiOjR9'
+powerbi_url = 'https://app.powerbi.com/view?r=eyJrIjoiZjI4NmNiN2QtNDMxOC00MmZjLTlkNWItN2FlMDdkMjZiOTRmIiwidCI6IjExZGJiZmUyLTg5YjgtNDU0OS1iZTEwLWNlYzM2NGU1OTU1MSIsImMiOjR9'
 
 st.components.v1.iframe(powerbi_url, width=800, height=600)
