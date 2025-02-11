7dc838a14e41d6e8ee3f97d3158a2a866afe6979
Marina Mendez <marina.mendez@quintoandar.com.br>
2025-02-11 23:11:41 +0000

    ajuste3

--- pages/02 Estudo.py
+++ pages/02 Estudo.py
@@ -12,10 +12,8 @@ from Funcoes import rodando_modelo # type: ignore
 # Configuração da página
 st.set_page_config(page_title= 'ONG - Passos Mágicos', layout='wide', page_icon=':gem:')
 
-#if __name__ == '__main__':
-        #apply_custom_style()
 
-st.markdown("""<style>[data-testid="stSidebar"] {display: flex; align-items: center; margin-bottom: 20px;}.icon-container a {margin-right: 15px;}</style>""", unsafe_allow_html=True)
+st.markdown("""<style>[data-testid="stSidebar"] {display: flex; align-items: center; margin-bottom: 5px;}.icon-container a {margin-right: 15px;}</style>""", unsafe_allow_html=True)
 with st.sidebar:
     st.markdown('''<b><u>Equipe FIAP - 5DTAT - Grupo 79</u></b></font>''', unsafe_allow_html=True)
     st.markdown('''
